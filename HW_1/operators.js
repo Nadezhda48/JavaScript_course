64cabf5f8797ec7a812c4fd748d98bfe5b38bac9
nadezhda.bilega <nadezhda.bilega@gmail.com>
2022-10-05 14:30:57 +0300

    rework homework_1

--- HW_1/operators.js
+++ HW_1/operators.js
@@ -1,8 +1,6 @@
-const value = 1;
-let textValue = '1';
 var userLevel;
 
-for(i= 0; i<=12; i++){
+for(let i = 0; i <= 12; i++){
     if (i <= 6) {
         userLevel = "Low level"
     } else if (i >= 7 && i <= 9) {
@@ -13,6 +11,11 @@ for(i= 0; i<=12; i++){
     console.log(userLevel)
 }
 
+//--------------------------
+
+const value = 1;
+const textValue = '1';
+
 if(value === textValue){
     console.log("The same value and type")
 } else if(value == textValue){
@@ -21,31 +24,68 @@ if(value === textValue){
     console.log("The values is a different")
 }
 
+//--------------------------
+
 const age = 12
 const verifyAge = age >= 18 ? "You can drive a car" : "You can*t drive a car"
 console.log(verifyAge)
+
+//--------------------------
 
 month = "March"
 
 switch(month){
     case "December":
     case 'January':
-    case 'February':{
+    case 'February':
         console.log("The season is winter")
-    } break
+        break;
     case 'March':
     case 'April':
-    case 'May':{
+    case 'May':
         console.log("The season is spring")
-    } break
+        break;
     case 'June':
     case 'July':
-    case 'August':{
+    case 'August':
         console.log("The season is summer")
-    } break
+        break;
     case 'September':
     case 'October':
-    case 'November':{
-            console.log("The season is autumn")
-    } break
+    case 'November':
+        console.log("The season is autumn")
+        break;
+    default:
+        console.error("Sorry, you entered month is wrong!")
+    
 }
+
+//--------------------------
+
+const footwear = 4200;
+const cap = 1300;
+const jacket = 2100;
+const blouse = 1000;
+const jeans = 1100;
+const birth = '29.02'
+const promoCodeName = 'happy'
+let goods = 0;
+
+goods += footwear * 2 + cap + jeans + jacket
+
+const discount = goods > 10000 || birth === '29.02'
+    ? 70 
+    : 10 
+
+    const promoCode = promoCodeName === 'happy'
+    ? 15 
+    : 0 
+
+const checkDiscount = discount + promoCode
+console.log(`Congadulation! Your discount is ${checkDiscount}%`)
+
+const priceInCheck = goods - (goods * checkDiscount / 100)
+console.log(`To be paid is ${priceInCheck} gruvnya`)
+
+
+
